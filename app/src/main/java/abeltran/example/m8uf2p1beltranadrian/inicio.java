99d679402891c3b7f656111f9971b953472b917c
Adrian Beltran <abeltran.insmollet4@gmail.com>
2020-04-02 10:10:36 +0200

    Añadido mensaje de despedida, al clicar boton de salir.

--- app/src/main/java/abeltran/example/m8uf2p1beltranadrian/inicio.java
+++ app/src/main/java/abeltran/example/m8uf2p1beltranadrian/inicio.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -43,6 +44,7 @@ public class inicio extends AppCompatActivity {
         salir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(inicio.this, "Hasta la proxima!", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
